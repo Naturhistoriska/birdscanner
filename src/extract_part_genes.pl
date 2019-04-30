bf988cb4539268eac53cff5e99beb79c7d8a3800
nylander <j.a.a.nylander@gmail.com>
2019-04-30 16:45:42 +0200

    added part extraction script

---
+++ src/extract_part_genes.pl
@@ -0,0 +1,153 @@
+#!/usr/bin/perl 
+#===============================================================================
+=pod
+
+
+=head2
+
+         FILE: extract_part_genes.pl
+
+        USAGE: ./extract_part_genes.pl  
+
+  DESCRIPTION: 
+
+      OPTIONS: ---
+
+ REQUIREMENTS: ---
+
+         BUGS: ---
+
+        NOTES: Should be run inside /home/nylander/run/pe/Jarvis_et_al_2014/FASTA_files_of_loci_datasets/Filtered_sequence_alignments/2516_Introns/2500orthologs
+               or passed the path to that folder as argument
+
+       AUTHOR: Johan Nylander (JN), johan.nylander@nbis.se
+
+      COMPANY: NBIS/NRM
+
+      VERSION: 1.0
+
+      CREATED: 2019-04-30 15:26:30
+
+     REVISION: ---
+
+=cut
+
+
+#===============================================================================
+
+use strict;
+use warnings;
+use Cwd;
+use File::Path qw( make_path );
+use File::Slurp;
+use Data::Dumper;
+use Parallel::ForkManager;
+use Getopt::Long;
+
+my $verbose = 1;
+my $cpu = 10;
+
+GetOptions(
+    "verbose!"  => \$verbose,
+    "help"      => sub { exec("perldoc", $0); exit(0); },
+);
+
+
+my $partfile = 'sate.removed.intron.noout-allgap.filtered.part';
+my $seqfile = 'sate.removed.intron.noout.aligned-allgap.filtered';
+
+my $wd = q{};
+
+if (@ARGV) {
+    $wd = shift(@ARGV);
+    if ($wd eq '.') {
+        $wd = getcwd;
+    }
+}
+else {
+    $wd = getcwd;
+}
+
+my @dirs = read_dir($wd);
+
+my $outfolder = $wd . '/' . 'part_fasta_files';
+
+make_path($outfolder);
+
+#my $pm = new Parallel::ForkManager($cpu);
+
+foreach my $dir (@dirs) {
+
+    next unless -d $dir;
+    next if $dir =~ /fasta/;
+
+    #my $pid = $pm->start and next;
+
+    ## Begin Parallel block
+        open my $PFILE, "$dir/$partfile" or die "$!";
+        sleep(1);
+        while (<$PFILE>) {
+            chomp:
+            my @F = split /\s+/, $_;
+            my $id = $F[1];
+            my ($start, $end) = split /-/, $F[3];
+            print STDERR "dir:$dir id:$id start:$start end:$end\n" if $verbose;
+            extract_fasta_coordinates($outfolder, $dir, $id, $seqfile, $start, $end, $id);
+        }
+        close($PFILE);
+    ## End Parallel block
+
+    #$pm->finish;
+}
+
+#$pm->wait_all_children;
+
+
+sub extract_fasta_coordinates {
+    my ($outfolder, $dir, $id, $seqfile, $start, $end, $outname) = @_;
+    my $fastafilepath = $outfolder . '/' . $id . '.' . $dir . '.fas';
+    my $seqfilepath = $dir . '/' . $seqfile;
+    open my $FAS, ">", $fastafilepath or die "$!";
+    open my $SEQ, "<", $seqfilepath or die "$!";
+    my $i = 0;
+    my $first = 0;
+    while (<$SEQ>) {
+        chomp;
+        if ($_ =~ /^\s*$/) {
+            next;
+        }
+        if (/^>/) {
+            if ($first) {
+                print $FAS "\n";
+            }
+            if ($outname) {
+                print $FAS ">$outname\n";
+            }
+            else {
+                print $FAS $_, "\n";
+            }
+            $i = 0;
+            $first = 1;
+        }
+        else {
+            my (@sequenceline) = split //;
+            foreach my $base (@sequenceline) {
+                $i++;
+                if ($end) {
+                    if ($i >= $start && $i <= $end) {
+                        print $FAS $base;
+                    }
+                }
+                else {
+                    if ($i >= $start) {
+                        print $FAS $base;
+                    }
+                }
+            }
+        }
+    }
+    close($SEQ);
+    close($FAS);
+}
+
+__END__
